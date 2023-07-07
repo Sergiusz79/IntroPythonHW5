8a857759af139b79dac9549314959540ffd69974
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-07-07 17:50:28 +0200

    028

--- Task028.py
+++ Task028.py
@@ -8,3 +8,32 @@
 
 # 2 2
 #     4
+
+def enter_num(text):
+    try:
+        a = int(input(f"Enter the {text} :> "))
+    except:
+        print("Error! This is not integer number!")
+    return a
+
+def rec(a, b):
+    if b == 0:
+        return a
+    elif b == 1:
+        return a + 1
+    else:
+        return rec(a, b-1) + 1
+
+
+def task():
+    a = enter_num('number')
+    b = enter_num('degree number')
+
+    print(rec(a, b))
+
+task()
+
+
+
+
+
